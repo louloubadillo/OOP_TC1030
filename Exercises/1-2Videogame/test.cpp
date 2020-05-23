0d01c6eb3b32c6bda87399e44e3935af8ee626a7
LouLouBadillo <lulu.badillo.csm@gmail.com>
2020-05-22 23:02:26 -0500

    tested

--- Exercises/1-2Videogame/test.cpp
+++ Exercises/1-2Videogame/test.cpp
@@ -8,11 +8,19 @@ int main(){
     Videogame* MarioKart = new Videogame("Mario Kart"); 
     Player* Lulu = new Player("Lulu"); 
     Player* Joaq = new Player("Joaq"); 
+    Player* Sergio = new Player("Sergio");
     MarioKart->setPlayer1(Lulu); 
     MarioKart->setPlayer2(Joaq); 
     MarioKart->startGame(); 
     Lulu->setScore(5);  
     Joaq->setScore(8); 
     MarioKart -> endGame(); 
+    Videogame* Smash = new Videogame("Smash"); 
+    Smash -> setPlayer1(Joaq); 
+    Smash -> setPlayer2(Sergio); 
+    Smash -> startGame(); 
+    Sergio-> setScore(200); 
+    Joaq -> setScore(180); 
+    Smash-> endGame(); 
     return 0; 
 }
